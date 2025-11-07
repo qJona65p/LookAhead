797bd578ec56714dab16bbf908267428b0909a73
Jonnhy <104951133+qJona65p@users.noreply.github.com>
2025-11-07 09:46:18 -0600

    Delete expo-camera-first-version directory

--- expo-camera-first-version/FaceDetectionProvider.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { FaceDetectionProvider } from '@infinitered/react-native-mlkit-face-detection';
-
-export default function FaceDetectionWrapper({ children }: { children: React.ReactNode }) {
-    return <FaceDetectionProvider>{ children }</FaceDetectionProvider>
-}
